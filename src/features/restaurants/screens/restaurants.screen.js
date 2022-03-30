badf61d6a2efeecd084b5bc8d4ab45aa36c080d9
Daniel Frank <dfrank@luxurypresence.com>
2022-03-30 11:00:13 -0500

    Remove background-color from RestaurantListContainer

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -17,7 +17,6 @@ const SearchContainer = styled.View`
 const RestaurantListContainer = styled.View`
   flex: 1;
   padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.secondary};
   flex-grow: 1;
 `;
 
