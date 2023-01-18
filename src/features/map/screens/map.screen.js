a7f8068e14c25e8a7751214347c0a1c6ce56524e
Daniel Frank <dfrank@luxurypresence.com>
2023-01-18 15:27:40 -0600

    Set up Callout for MapMarkers

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,15 +1,19 @@
 import React, { useContext, useState, useEffect } from "react";
-import MapView, { Marker } from "react-native-maps";
+import { View } from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
 import styled from "styled-components/native";
 
 import { Search } from "../components/search.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { LocationContext } from "../../../services/location/location.context";
+import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
+
+const SomeText = styled.Text``;
 
 export const MapScreen = () => {
   const { restaurants } = useContext(RestaurantsContext);
@@ -44,7 +48,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <Callout>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
           );
         })}
       </Map>
